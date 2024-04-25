d314e9b136d8c6cb4c442ad78f9190e1768d8d6d
mautonomy <sonya.ro@tutanota.com>
2024-04-25 19:42:21 +0300

    numpy test env

---
+++ myenv/numpy_.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = np.array([20, 30, 40, 50])
+b = np.arange(4)
+
+c = a - b
+
+print(c)
+
+b**2
+
+print(b**2)
+
+10 * np.sin(a)
+print(10 * np.sin(a))
+
+a < 35
+print(a < 35)
+
+print(np.__version__)
